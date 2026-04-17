b68dfe77bd21645eaa0dc3096f8ac024221a1f5a
Callioper <407811164@qq.com>
2026-04-18 04:30:23 +0800

    fix: remove all ztoolkit references, use native Zotero APIs
    - Removed ColumnOptions, DialogHelper imports from addon.ts
    - Removed createZToolkit() call and ztoolkit data property
    - Set env directly to 'production' instead of __env__
    - Emptied ztoolkit.ts utility file
    - Removed ztoolkit, ZToolkit, __env__ type declarations

--- src/addon.ts
+++ src/addon.ts
@@ -1,24 +1,12 @@
 import { config } from "../package.json";
-import { ColumnOptions, DialogHelper } from "zotero-plugin-toolkit";
 import hooks from "./hooks";
-import { createZToolkit } from "./utils/ztoolkit";
 
 class Addon {
   public data: {
     alive: boolean;
     config: typeof config;
-    env: "development" | "production";
+    env: "production" | "development";
     initialized?: boolean;
-    ztoolkit: ZToolkit;
-    locale?: {
-      current: any;
-    };
-    prefs?: {
-      window: Window;
-      columns: Array<ColumnOptions>;
-      rows: Array<{ [dataKey: string]: string }>;
-    };
-    dialog?: DialogHelper;
   };
   public hooks: typeof hooks;
   public api: object;
@@ -27,9 +15,8 @@ class Addon {
     this.data = {
       alive: true,
       config,
-      env: __env__,
+      env: "production",
       initialized: false,
-      ztoolkit: createZToolkit(),
     };
     this.hooks = hooks;
     this.api = {};
--- src/utils/ztoolkit.ts
+++ src/utils/ztoolkit.ts
@@ -1,13 +1,2 @@
-import { ZoteroToolkit } from "zotero-plugin-toolkit";
-import { config } from "../../package.json";
-
-export function createZToolkit() {
-  const ztoolkit = new ZoteroToolkit();
-  ztoolkit.basicOptions.log.prefix = `[${config.addonName}]`;
-  ztoolkit.basicOptions.api.pluginID = config.addonID;
-  ztoolkit.ProgressWindow.setIconURI(
-    "default",
-    `chrome://${config.addonRef}/content/icons/favicon.png`,
-  );
-  return ztoolkit;
-}
+// This file is kept for compatibility but no longer exports anything
+// All toolkit functionality has been removed
