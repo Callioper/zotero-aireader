4534d7ac1db3324dda8c6411ef81e8d6e04120b2
Eclaw <eclaw@example.com>
2026-04-18 11:39:31 +0800

    fix: remove top-level await which is not supported by IIFE format

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ if (!Zotero[config.addonInstance]) {
   // @ts-ignore - Plugin instance is not typed
   Zotero[config.addonInstance] = new Addon();
   // @ts-ignore - Plugin instance is not typed
-  await Zotero[config.addonInstance].hooks.onStartup();
+  Zotero[config.addonInstance].hooks.onStartup();
 }
